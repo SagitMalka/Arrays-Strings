0f7ed4283705f9407354f6c854552f7a7627c010
sagit <sagit@ubuntu.myguest.virtualbox.org>
2023-01-08 16:21:40 +0200

    add decumentation

--- isort.c
+++ isort.c
@@ -1,49 +1,67 @@
-#include <stdio.h>
-#define SIZE_OF_ARRAY 50
 /**
- * insertion sort
-*/
+ * INSERTION SORT
+ *
+ * use pointers only, execlude declerintion
+ * this program handels specific case of 50 inteegers only
+ */
+
+#include <stdio.h>
+
+#define SIZE_OF_ARRAY 50
 
 /**
     @brief shifts i elements by one to right
     @param arr pointer to some place in array
     @param i number of elements to shift
  */
-void shift_element (int *arr, int i){
+void shift_element(int *arr, int i)
+{
     int n;
-    for(n = i-1; n>=0; n--){
-        *(arr+n+1) = *(arr+n);
+    for (n = i - 1; n >= 0; n--)
+    {
+        *(arr + n + 1) = *(arr + n);
     }
 }
 
-void insertion_sort(int* arr, int len){
+/**
+    @brief insertion sort use help function shift_elements
+    @param arr pointer to array
+    @param len length of array
+*/
+void insertion_sort(int *arr, int len)
+{
     int i, j;
-    for ( i = 1; i<len; i++){
-        int key = *(arr +i);
-        for ( j = i-1 ; j >=0 ; j--) {
-            if( *(arr+j) > key){
-                shift_element(arr+j, 1);
+    for (i = 1; i < len; i++)
+    {
+        int key = *(arr + i);
+        for (j = i - 1; j >= 0; j--)
+        {
+            if (*(arr + j) > key)
+            {
+                shift_element(arr + j, 1);
             }
-            else{
+            else
+            {
                 break;
             }
         }
-        *(arr +j+1 ) = key;
+        *(arr + j + 1) = key;
     }
 }
 
+int main()
+{
 
-
-int main(){
     int array[SIZE_OF_ARRAY];
-    for(int i =0; i<SIZE_OF_ARRAY; i++){
-        scanf("%d", array+i);
+    for (int i = 0; i < SIZE_OF_ARRAY; i++)
+    {
+        scanf("%d", array + i);
     }
     insertion_sort(array, SIZE_OF_ARRAY);
-    for (int i = 0; i < SIZE_OF_ARRAY-1; i++)
+    for (int i = 0; i < SIZE_OF_ARRAY - 1; i++)
     {
         printf("%d,", array[i]);
     }
-    printf("%d\n", array[SIZE_OF_ARRAY-1]);
+    printf("%d\n", array[SIZE_OF_ARRAY - 1]);
     return 0;
 }
--- txtfind.c
+++ txtfind.c
@@ -1,6 +1,11 @@
-//
-// Created by sagit on 1/7/23.
-//
+/**SEARCH IN TEXT USE STRING METHODS
+ * 
+ * @input text. first line: string and char 'a' or 'b'
+ * @param string would be word_to_find
+ * @param char dedcate function to run
+ * second line is empty, and third-to-end is text to search in 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,7 +16,7 @@
 int checkWord(char *word_to_find, int wtf_size, char *word, int word_size);
 int containsString(char *line, char *substring);
 
-// function a: print all lines contains specific substring
+/** function a: print all lines contains specific substring */
 void printLinesWith(char *word_to_find) {
     char line[LINE];
 
@@ -23,7 +28,7 @@ void printLinesWith(char *word_to_find) {
     }
 }
 
-//function b: find & print all words similar to string
+/** function b: find & print all words similar to string */
 void printWordsWith(char *word_to_find) {
     char buf[WORD];
 
@@ -63,6 +68,11 @@ int main() {
     return 0;
 }
 
+
+
+
+
+/**help function: find all lines contains specific substring*/
 int containsString(char *line, char *substring) {
     unsigned long line_length = strlen(line);
     unsigned long substring_length = strlen(substring);
@@ -82,6 +92,7 @@ int containsString(char *line, char *substring) {
     return 0;
 }
 
+/**help function: compare string's length and contance*/
 int checkWord(char *word_to_find, int wtf_size, char *word, int word_size) {
     if (wtf_size != word_size && wtf_size + 1 != word_size) {
         return -1;
